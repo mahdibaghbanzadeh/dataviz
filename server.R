3167e644ce2904abbbf4426fb71bb3ce61fdb065
Mahdi Baghbanzadeh <36835068+mahdibaghbanzadeh@users.noreply.github.com>
2023-05-08 12:33:53 -0400

    Add files via upload

---
+++ server.R
@@ -0,0 +1,113 @@
+library(shiny)
+library(tidyverse)
+library(cowplot)
+
+# Define server logic required to draw a histogram
+function(input, output, session) {
+  plot_list <- reactive({
+    readRDS('top_plots.RDS')
+  })
+  output$metabolites <- renderUI({
+    plot_names = names(plot_list())
+    print(plot_names)
+    selectizeInput(inputId = 'met', label = 'Select metabolite',
+                choices = plot_names, multiple = TRUE,
+                options = list(maxItems = 4),
+                selected = plot_names[1:4])
+  })
+
+    output$boxPlot <- renderPlot({
+      req(input$met)
+      ln = length(input$met)
+      # The palette with grey:
+      cbPalette <- c("#E69F00", "#56B4E9", "#009E73",
+                              "#F0E442", "#0072B2", 
+                              "#D55E00", "#CC79A7", "#999999")
+                              
+    if(ln >2){
+      x = c(0,.45, 0, .45)
+      
+      fig_comb <- ggdraw() +
+        draw_plot(plot_list()[[input$met[1]]]+
+                    scale_fill_manual(values = cbPalette[1:5])+
+                    scale_x_discrete(labels=c("G7 (n=6)", "G11 (n=7)",
+                                              "G9 (n=5)","G8 (n=6)",
+                                              "G10 (n=6)"))+
+                    theme(
+                      axis.title.x = element_text(size = input$xtitle),
+                      axis.text.x = element_text(size = input$xlab),
+                      axis.title.y = element_text(size = input$ytitle),
+                      axis.text.y = element_text(size = input$ylab)),
+                  x = 0, y = 0.5, width = .35, height = .45)+
+        draw_plot(plot_list()[[input$met[2]]]+
+                    scale_fill_manual(values = cbPalette[1:5])+
+                    scale_x_discrete(labels=c("G7 (n=6)", "G11 (n=7)",
+                                              "G9 (n=5)","G8 (n=6)",
+                                              "G10 (n=6)"))+
+                    theme(
+                      axis.title.x = element_text(size = input$xtitle),
+                      axis.text.x = element_text(size = input$xlab),
+                      axis.title.y = element_text(size = input$ytitle),
+                      axis.text.y = element_text(size = input$ylab)),
+                  x = 0.45, y = 0.5, width = .35, height = .45)
+      for(i in 3:4){
+        fig_comb <- fig_comb+
+          draw_plot(plot_list()[[input$met[i]]]+
+                      scale_fill_manual(values = cbPalette[1:5])+
+                      scale_x_discrete(labels=c("G7 (n=6)", "G11 (n=7)",
+                                                "G9 (n=5)","G8 (n=6)",
+                                                "G10 (n=6)"))+
+                      theme(
+                        axis.title.x = element_text(size = input$xtitle),
+                        axis.text.x = element_text(size = input$xlab),
+                        axis.title.y = element_text(size = input$ytitle),
+                        axis.text.y = element_text(size = input$ylab)),
+                    x = x[i], y = 0, width = .35, height = .45)
+        
+        
+    }
+    }else{
+        if(length(input$met)==1){
+          fig_comb <- ggdraw() +
+            draw_plot(plot_list()[[input$met[1]]]+
+                        scale_fill_manual(values = cbPalette[1:5])+
+                        scale_x_discrete(labels=c("G7 (n=6)", "G11 (n=7)",
+                                                  "G9 (n=5)","G8 (n=6)",
+                                                  "G10 (n=6)"))+
+                        theme(
+                          axis.title.x = element_text(size = input$xtitle),
+                          axis.text.x = element_text(size = input$xlab),
+                          axis.title.y = element_text(size = input$ytitle),
+                          axis.text.y = element_text(size = input$ylab)),
+                      x = 0, y = 0, width = 1, height = 1)
+        }else{
+          fig_comb <- ggdraw() +
+            draw_plot(plot_list()[[input$met[1]]]+
+                        scale_fill_manual(values = cbPalette[1:5])+
+                        scale_x_discrete(labels=c("G7 (n=6)", "G11 (n=7)",
+                                                  "G9 (n=5)","G8 (n=6)",
+                                                  "G10 (n=6)"))+
+                        theme(
+                          axis.title.x = element_text(size = input$xtitle),
+                          axis.text.x = element_text(size = input$xlab),
+                          axis.title.y = element_text(size = input$ytitle),
+                          axis.text.y = element_text(size = input$ylab)),
+                      x = 0, y = 0, width = .35, height = 1)+
+            draw_plot(plot_list()[[input$met[2]]]+
+                        scale_fill_manual(values = cbPalette[1:5])+
+                        scale_x_discrete(labels=c("G7 (n=6)", "G11 (n=7)",
+                                                  "G9 (n=5)","G8 (n=6)",
+                                                  "G10 (n=6)"))+
+                        theme(
+                          axis.title.x = element_text(size = input$xtitle),
+                          axis.text.x = element_text(size = input$xlab),
+                          axis.title.y = element_text(size = input$ytitle),
+                          axis.text.y = element_text(size = input$ylab)),
+                      x = 0.5, y = 00, width = .35, height = 1)
+        }
+      }
+      fig_comb
+      
+    })
+
+}
---
+++ ui.R
@@ -0,0 +1,27 @@
+library(shiny)
+
+
+# Define UI for application that draws a histogram
+fluidPage(
+
+    # Application title
+    titlePanel("Data Visualization, PUBH 6899"),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+            uiOutput('metabolites'),
+            sliderInput(inputId = 'xlab', label = 'Modify X-lab size',
+                        min = 6, max = 20, step = 1, value = 10),
+            sliderInput(inputId = 'ylab', label = 'Modify Y-lab size',
+                        min = 6, max = 20, step = 1, value = 10),
+            sliderInput(inputId = 'ytitle', label = 'Modify Y-title size',
+                        min = 6, max = 20, step = 1, value = 12)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            plotOutput('boxPlot')
+        )
+    )
+)
